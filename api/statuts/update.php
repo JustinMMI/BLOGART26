dcc94bd7e5ce46e961dec86b3aa6ae6343b34924
theo-7m45 <theo.messean.pro@gmail.com>
2026-01-29 18:29:39 +0100

    ViewsStatutsEdit/APIStatutsUpdate OK

--- api/statuts/update.php
+++ api/statuts/update.php
@@ -2,10 +2,10 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once '../../functions/ctrlSaisies.php';
 
-$libStat = ($_POST['libstat']);
-$numStat = ($_POST['numstat']);
+$libStat = ($_POST['libStat']);
+$numStat = ($_POST['numStat']);
 
-sql_update('STATUT',"libstat= '$libStat'","numStat = $numStat" );
+sql_update('STATUT',"libStat= '$libStat'","numStat = '$numStat'" );
 
 
 header('Location: ../../views/backend/statuts/list.php');
--- views/backend/statuts/edit.php
+++ views/backend/statuts/edit.php
@@ -1,6 +1,11 @@
 <?php
 include '../../../header.php';
 
+if (!isset($_SESSION['user']) || $_SESSION['user']['statut'] !== 'Administrateur') {
+    header('Location: /');
+    exit;
+}
+
 if(isset($_GET['numStat'])){
     $numStat = (int)$_GET['numStat'];
     $libStat = sql_select("STATUT", "libStat", "numStat = $numStat")[0]['libStat'];
@@ -14,13 +19,33 @@ if(isset($_GET['numStat'])){
     </div>
     <div class="col-md-12">
         <!-- Form to create a new statut -->
-        <form action="<?php echo ROOT_URL . '/api/statuts/update.php' ?>" method="post">
-            <div class="form-group">
-                <label for="libStat">Nom du statut</label>
-                <input id="numStat" name="numStat" class="form-control" style="display: none" type="text" value="<?php echo($numStat); ?>" readonly="readonly" />
-                <input id="libStat" name="libStat" class="form-control" type="text" value="<?php echo($libStat); ?>" readonly="readonly" disabled />
-            </div>
-            <br />
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Modifier un statut</h1>
+        </div>
+        <div class="col-md-12">
+            <form action="<?php echo ROOT_URL . '/api/statuts/update.php' ?>" method="post">
+                <!-- Numéro -->
+                <div class="form-group">
+                    <label for="libStat">Numero</label>
+                    <input id="numStat" name="numStat" class="form-control" style="display: none" type="text" value="<?php echo($numStat); ?>" readonly="readonly" />
+                    <input id="numStat" name="numStat" class="form-control" type="text" value="<?php echo($numStat); ?>" readonly="readonly"  />
+                </div>
+
+                <!-- Date de création -->
+                <div class="form-group">
+                    <label for="libStat">Date de création</label>
+                    <input id="numStat" name="numStat" class="form-control" style="display: none" type="text" value="<?php echo($numStat); ?>" readonly="readonly" />
+                    <input id="libStat" name="libStat" class="form-control" type="text" value="<?php echo($libStat); ?>" readonly="readonly"  />
+                </div>
+
+                <!-- Libellé -->
+                <div class="form-group">
+                    <label for="libStat">Libellé </label>
+                    <input id="libStat" name="libStat" class="form-control" type="text" autofocus="autofocus" />
+                </div>
             <div class="form-group mt-2">
                 <a href="list.php" class="btn btn-primary">List</a>
                 <button type="submit" class="btn btn-danger">Confirmer Edit ?</button>
