53a6e1d3d3bbb501fb057d6ad05e7839eef0b77f
JuJust123 <justin.esquer@mmibordeaux.com>
2026-01-29 17:42:27 +0100

    Thematique v1

--- views/backend/thematiques/create.php
+++ views/backend/thematiques/create.php
@@ -1,3 +1,34 @@
 <?php
 include '../../../header.php';
 
+// Sécurité admin
+if (!isset($_SESSION['user']) || $_SESSION['user']['statut'] !== 'Administrateur') {
+    header('Location: /');
+    exit;
+}
+
+// Charger les thématiques
+$thematiques = sql_select("THEMATIQUE", "*");
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Liste des Thématiques</h1>
+        </div>
+
+        <div class="col-md-12">
+            <form action="<?php echo ROOT_URL . '/api/thematiques/create.php'; ?>" method="post">
+
+                <div class="form-group">
+                    <label>Titre</label>
+                    <input name="libThem" class="form-control" required>
+                </div>
+
+                <br>
+                <a href="list.php" class="btn btn-primary">List</a>
+                <button type="submit" class="btn btn-success">Confirmer create ?</button>
+            </form>
+        </div>
+    </div>
+</div>
