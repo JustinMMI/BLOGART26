d060cc67589d8b36acc6db307020739a407d118a
lisabruno <lisa.bruno@mmibordeaux.com>
2026-01-30 09:10:52 +0100

    delete keywords

--- api/keywords/delete.php
+++ api/keywords/delete.php
@@ -1,2 +1,11 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+$numMotCle = (int) $_POST['numMotCle'];
+
+sql_delete('MOTCLE', "numMotCle = $numMotCle");
+
+header('Location: ../../views/backend/keywords/list.php');
+exit;
 
--- views/backend/keywords/delete.php
+++ views/backend/keywords/delete.php
@@ -1,9 +1,9 @@
 <?php
 include '../../../header.php';
 
-if(isset($_GET['numStat'])){
-    $numStat = $_GET['numStat'];
-    $libStat = sql_select("STATUT", "libStat", "numStat = $numStat")[0]['libStat'];
+if(isset($_GET['numMotCle'])){
+    $numMotCle = $_GET['numMotCle'];
+    $libMotCle = sql_select("MOTCLE", "libMotCle", "numMotCle = $numMotCle")[0]['libMotCle'];
 }
 ?>
 
@@ -11,15 +11,15 @@ if(isset($_GET['numStat'])){
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Suppression Statut</h1>
+            <h1>Suppression Mot Clé</h1>
         </div>
         <div class="col-md-12">
             <!-- Form to create a new statut -->
-            <form action="<?php echo ROOT_URL . '/api/statuts/delete.php' ?>" method="post">
+            <form action="<?php echo ROOT_URL . '/api/keywords/delete.php' ?>" method="post">
                 <div class="form-group">
-                    <label for="libStat">Nom du statut</label>
-                    <input id="numStat" name="numStat" class="form-control" style="display: none" type="text" value="<?php echo($numStat); ?>" readonly="readonly" />
-                    <input id="libStat" name="libStat" class="form-control" type="text" value="<?php echo($libStat); ?>" readonly="readonly" disabled />
+                    <label for="libMotCle">Nom du mot clé</label>
+                    <input id="numMotCle" name="numMotCle" class="form-control" style="display: none" type="text" value="<?php echo($numMotCle); ?>" readonly="readonly" />
+                    <input id="libMotCle" name="libMotCle" class="form-control" type="text" value="<?php echo($libMotCle); ?>" readonly="readonly" disabled />
                 </div>
                 <br />
                 <div class="form-group mt-2">
